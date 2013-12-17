c8886619bbbfcf441e5e929c3b2877391ffa2a64
Nicholas Paun <np@icebergsystems.ca>
2013-12-17 08:28:45 -0800

    DB extension: common name: prussic acid = HCN

--- fact/retained.pl
+++ fact/retained.pl
@@ -9,3 +9,4 @@ retained(["N","H"|Rest],Rest,[["N",1],["H",3]]) --> "ammonia".
 common(["Na","H","C","O"|Rest],Rest,[["Na",1],[[["H",1],["C",1],["O",3]],1]]) --> "baking soda".
 common(["C","H","O"|Rest],Rest,[["C",3],["H",8],["O",1]]) --> "rubbing alcohol".
 common(["C","H","O"|Rest],Rest,[["C",3],["H",8],["O",1]]) --> "surgical spirit".
+common(["H","C","N"|Rest],Rest,[["H",1],[[["C",1],["N",1]],1]]) --> "prussic acid". %% Ionic form of CN is used so the acid recognizer will kick in.
