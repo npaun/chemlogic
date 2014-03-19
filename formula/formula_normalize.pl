da7d54ac7849120eb892333ce1b36da6c35524a2
Nicholas Paun <np@icebergsystems.ca>
2014-03-19 14:12:53 -0700

    Fixed an issue; element no longer blows up on those ion/compounds, the program can now clearly explain what is wrong with (NH4)2.

--- formula/formula_normalize.pl
+++ formula/formula_normalize.pl
@@ -1,7 +1,7 @@
 :- set_prolog_flag(double_quotes,chars).
 
 formula(Fmt,Elems,ElemsR,Formula,FormulaR) --> 
-	formula_part_first(Fmt,Elems,ElemsR0,Formula,FormulaR0),
+	(formula_part_first(Fmt,Elems,ElemsR0,Formula,FormulaR0); syntax_stop(part_first)),
 	(hydrate_part(Fmt,ElemsR0,ElemsR,FormulaR0,FormulaR), !).
 
 % hydrate_part(_,[],[],[],[],[],[]) :- !.
@@ -19,15 +19,17 @@ formula_part_first(Fmt,Elems,ElemsR,Part,PartR) -->
 	(formula_part(Fmt,Type,ElemsR0,ElemsR,PartR0,PartR), !).
 
 formula_part(_,none,[],[],[],[],[],[]).
+formula_part(_,multi,[],[],[],[],[],[]) :- !, fail.
 formula_part(Fmt,multi,Elems,ElemsR,Part,PartR) --> part(Fmt,_,Elems,ElemsR0,Part,PartR0), (formula_part(Fmt,_,ElemsR0,ElemsR,PartR0,PartR), !).
 formula_part(_,none,Elems,Elems,Part,Part) --> [].
 
 part(Fmt,multi,Elems,ElemsR,[[Sym,Num]|PartR],PartR) --> 
 	"(", (group_symbol(Fmt,Elems,ElemsR,Sym); ({var(Sym)} -> syntax_stop(group))), ")",
-	(num_decimal(Num); syntax_stop(number)).
+	(num_decimal(Num), !; syntax_stop(number)).
 
 part(Fmt,multi,Elems,ElemsR,[[Sym,1]|PartR],PartR) --> group_symbol(Fmt,Elems,ElemsR,Sym).
-part(Fmt,_,[Elem|ElemsR],ElemsR,[[Elem,Num]|PartR],PartR) --> (element_symbol(Elem); syntax_stop(element)), ((subscript(Fmt,Num); syntax_stop(number)), !).
+part(Fmt,_,[Elem|ElemsR],ElemsR,[[Elem,Num]|PartR],PartR) --> ( element_symbol(Elem); syntax_stop(element)), (subscript(Fmt,Num), !).
+
 
 subscript(_,Num) --> { nonvar(Num) -> Num = 1}, [].
 subscript(Fmt,Num) --> output(Fmt,sub_start), num_decimal(Num), output(Fmt,sub_end).
