d09a980131cbc018732a0702b814c128608b9469
Nicholas Paun <np@icebergsystems.ca>
2014-03-25 10:25:52 -0700

    Good enough, so far. Now to the unscrewing.

--- io/tokenizer.pl
+++ io/tokenizer.pl
@@ -1,13 +1,18 @@
 :- use_module(library(dcg/basics)).
 :- set_prolog_flag(double_quotes,chars).
 
-atomics_to_chars([],[]).
 
 atomics_to_chars([Atom|AtomS],[Chars|CharsS]) :-
 	atom_chars(Atom,Chars),
 	atomics_to_chars(AtomS,CharsS).
 
+atomics_to_chars([],[]).
+
 tokenizer(In,Out) :-
 	atomic_list_concat(AtomsRaw,' ',In),
 	delete(AtomsRaw,'',Atoms),
 	atomics_to_chars(Atoms,Out).
+
+outputter(In,Out) :-
+	atomics_to_chars(Atoms,In),
+	atomic_list_concat(Atoms,' ',Out).
