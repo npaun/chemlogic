0d221ddf6e494c52a1db7e7bd14c2631bd36ecc4
Nicholas Paun <np@icebergsystems.ca>
2014-03-28 13:25:56 -0700

    A try to explain a problem.

--- balance/solve_error.pl
+++ balance/solve_error.pl
@@ -1,3 +1,17 @@
+count([],X,0).
+count([X|T],X,Y):- count(T,X,Z), Y is 1+Z.
+count([X1|T],X,Z):- X1\=X,count(T,X,Z).
+
+countall(List,X,C) :-
+	sort(List,List1),
+	member(X,List1),
+	count(List,X,C).
+
+solve_explain(Matrix,(Elems,_)) :-
+	countall(Elems,Elem,Count),
+	Count < 2.
+
+
 solve_handler(Matrix,Coeffs,Data) :-
 	catch(
 		solve_run(Matrix,Coeffs,Data),
