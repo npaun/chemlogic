6fd640baed193762fbca0fff6662da28f62ee7cd
Nicholas Paun <np@icebergsystems.ca>
2014-03-28 11:45:54 -0700

    Added more retained names because I do not feel like getting to work.

--- fact/retained.pl
+++ fact/retained.pl
@@ -12,3 +12,7 @@ common(["C","H","O"|Rest],Rest,[["C",3],["H",8],["O",1]]) --> "surgical spirit".
 common(["H","C","N"|Rest],Rest,[["H",1],[[["C",1],["N",1]],1]]) --> "prussic acid". %% Ionic form of CN is used so the acid recognizer will kick in.
 common(["H","C","O"|Rest],Rest,[["H",1],[[["C",1],["H",3],["C",1],["O",1],["O",1]],1]]) --> "vinegar".
 common(["O"|Rest],Rest,[["O",3]]) --> "ozone".
+common(["Ca","C","O"|Rest],Rest,[["Ca",1],[[["C",1],["O",3]],1]]) --> "chalk".
+common(["Ca","C","O"|Rest],Rest,[["Ca",1],[[["C",1],["O",3]],1]]) --> "limestone".
+common(["Ca","O"|Rest],Rest,[["Ca",1],["O",1]]) --> "quicklime".
+common(["Ca","O","H"|Rest],Rest,[["Ca",1],[[["O",1],["H",1]],2]]) --> "slaked lime".
