1f0f7cd4e6cef8aa2e36caaab90d88ffa29f3688
Nicholas Paun <np@icebergsystems.ca>
2014-03-28 11:37:33 -0700

    Added some more common names of chemicals

--- fact/retained.pl
+++ fact/retained.pl
@@ -10,3 +10,5 @@ common(["Na","H","C","O"|Rest],Rest,[["Na",1],[[["H",1],["C",1],["O",3]],1]]) --
 common(["C","H","O"|Rest],Rest,[["C",3],["H",8],["O",1]]) --> "rubbing alcohol".
 common(["C","H","O"|Rest],Rest,[["C",3],["H",8],["O",1]]) --> "surgical spirit".
 common(["H","C","N"|Rest],Rest,[["H",1],[[["C",1],["N",1]],1]]) --> "prussic acid". %% Ionic form of CN is used so the acid recognizer will kick in.
+common(["H","C","O"|Rest],Rest,[["H",1],[[["C",1],["H",3],["C",1],["O",1],["O",1]],1]]) --> "vinegar".
+common(["O"|Rest],Rest,[["O",3]]) --> "ozone".
