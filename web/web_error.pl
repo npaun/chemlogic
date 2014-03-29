faa204b4d5870a7f5e79599107c29aadf300e3e8
Nicholas Paun <np@icebergsystems.ca>
2014-03-28 17:35:28 -0700

    Correctly indented web_error and fixed class names.

--- web/web_error.pl
+++ web/web_error.pl
@@ -1,10 +1,26 @@
 :- module(web_error,[error_handler/2]).
 
 highlight_error(highlight(Start,Token,Rest),HighlightHTML) :-
-	HighlightHTML = p(id(errorline),[span(Start),span(id(errortoken),Token),span(Rest)]).
+	HighlightHTML = p(class('error-line'),
+		[
+			span(Start),
+			span(class('error-token'),Token),
+			span(Rest)
+		]
+	).
 
 message_show(message(MessageErrcode,MessageUnparsed,Errcode),MessageHTML) :-
-	MessageHTML = div(id(errormessage),[pre(MessageErrcode),pre([MessageUnparsed,span(id(errcode),Errcode)])]).
+	MessageHTML = div(class('error-message'),
+		[
+			pre(MessageErrcode),
+			pre(
+				[
+					MessageUnparsed,
+					span(class(errcode),Errcode)
+				]
+			)
+		]
+	).
 
 error_handler([HighlightHTML,MessageHTML],[HighlightStruct,MessageStruct]) :-
 	highlight_error(HighlightStruct,HighlightHTML),
