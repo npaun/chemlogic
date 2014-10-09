c0193dde1efd9db0d3a40ed732a936a3e70b1c1a
Nicholas Paun <np@icebergsystems.ca>
2014-10-09 13:45:06 -0700

    [repo] Moved shebang to first line of shell scripts -- duh

--- .repo/setup-np.sh
+++ .repo/setup-np.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 #setup-np.sh: A script for me to use to configure commit tracking and pushing to two origins
 #(C) Copyright 2014 Nicholas Paun
-#!/bin/sh
 
 TOPLEVEL=`git rev-parse --show-toplevel`
 SETUP="$TOPLEVEL/.repo/setup.sh"
