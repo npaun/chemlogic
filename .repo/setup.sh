9c05c9e0b2b9782960d368205dcfeaf5a1f47b7e
Nicholas Paun <np@icebergsystems.ca>
2014-10-19 15:29:11 -0700

    Autogenerate Ctags on commit

--- .repo/setup.sh
+++ .repo/setup.sh
@@ -6,5 +6,8 @@ GITDIR=`git rev-parse --git-dir`
 echo "Enabling post-commit hook"
 echo "#!/bin/sh" >> $GITDIR/hooks/post-commit
 echo >> $GITDIR/hooks/post-commit
+
+echo "\$GIT_DIR/../.repo/runtags &" >> $GITDIR/hooks/post-commit
+
 chmod u+x $GITDIR/hooks/post-commit
 
