641ba7377e9704b143e1eb5ff627e33a3db82ca3
Nicholas Paun <np@icebergsystems.ca>
2014-12-27 15:35:48 -0800

    Added the ANSI output format and changed the CLI error handler to use the output formatting system to highlight errors.

--- io/format_out.pl
+++ io/format_out.pl
@@ -11,6 +11,7 @@
 symbol(user,arrow) --> " --> ".
 symbol(html,arrow) --> " &rarr; ".
 symbol(latex,arrow) --> " \\rightarrow ".
+symbol(ansi,arrow) --> " -->".
 
 symbol(user,sub_start) --> "".
 symbol(user,sub_end) --> "".
@@ -21,9 +22,26 @@ symbol(html,sub_end) --> "</sub>".
 symbol(latex,sub_start) --> "_{".
 symbol(latex,sub_end) --> "}".
 
+symbol(ansi,sub_start) --> "".
+symbol(ansi,sub_end) --> "".
+
+
 symbol(user,dot) --> ".".
 symbol(html,dot) --> "&middot;".
 symbol(latex,dot) --> "\\cdot".
+symbol(ansi,dot) --> ".".
+
+symbol(user,err_token_start) --> "".
+symbol(user,err_token_end) --> "".
+/*
+HTML error formatting is performed directly in the Web interface, to allow for CSS styling.
+LaTeX formatting is not intended to be used in an user-interactive program, so error highlighting is probably not necessary.
+	It may be added in the future.
+*/
+
+symbol(ansi,err_token_start) --> "\e[01;41;37m".
+symbol(ansi,err_token_end) --> "\e[00m".
+
 
 /** set_output_format(+Format) is det.
 
