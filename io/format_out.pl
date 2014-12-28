d3118eb15b0d3233f1e030d150762d18568ce04a
Nicholas Paun <np@icebergsystems.ca>
2014-12-28 13:01:44 -0800

    Added the html_android_textview output format, which will allow for formatted text in the App.

--- io/format_out.pl
+++ io/format_out.pl
@@ -10,6 +10,7 @@
 
 symbol(user,arrow) --> " --> ".
 symbol(html,arrow) --> " &rarr; ".
+symbol(html_android_textview,arrow) --> " &rarr; ".
 symbol(latex,arrow) --> " \\rightarrow ".
 symbol(ansi,arrow) --> " --> ".
 
@@ -18,6 +19,9 @@ symbol(user,sub_end) --> "".
 
 symbol(html,sub_start) --> "<sub>".
 symbol(html,sub_end) --> "</sub>".
+
+symbol(html_android_textview,sub_start) --> "<sub>".
+symbol(html_android_textview,sub_end) --> "</sub>".
 
 symbol(latex,sub_start) --> "_{".
 symbol(latex,sub_end) --> "}".
@@ -28,6 +32,7 @@ symbol(ansi,sub_end) --> "".
 
 symbol(user,dot) --> ".".
 symbol(html,dot) --> "&middot;".
+symbol(html_android_textview,dot) --> "&middot;".
 symbol(latex,dot) --> "\\cdot".
 symbol(ansi,dot) --> ".".
 
@@ -38,6 +43,13 @@ HTML error formatting is performed directly in the Web interface, to allow for C
 LaTeX formatting is not intended to be used in an user-interactive program, so error highlighting is probably not necessary.
 	It may be added in the future.
 */
+
+/* 
+ANDROID: TextView supports only some HTML tags via the HTML class. The font tag can change the text's color (set to orange), but not the background color (bgcolor, set to red). 
+I might extend the class to support this.
+*/
+symbol(html_android_textview,err_token_start) --> "<font color='#ffa500' bgcolor='#ff0000'>".
+symbol(html_android_textview,err_token_end) --> "</font>".
 
 symbol(ansi,err_token_start) --> "\e[01;41;37m".
 symbol(ansi,err_token_end) --> "\e[00m".
