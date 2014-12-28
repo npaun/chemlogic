76b5fd3eb953ee2c83389fd9cd155729e29f4f91
Nicholas Paun <np@icebergsystems.ca>
2014-12-27 16:31:18 -0800

    Moved print rule back to the main DSL file and removed the echo predicate. I do not know which method is better, but perhaps print can be used in DSL programs to communicate with the user.

--- cli/api.pl
+++ cli/api.pl
@@ -1,20 +1,6 @@
-:- op(990,xfy,::).
-:- op(100,xf,print).
-
-InputType - Input :: print :-
-        InputType - Input :: symbolic - Result,
-        writeln(Result),
-        !.
-
-InputType - Input :: ResultType print :-
-        InputType - Input :: ResultType - Result,
-        writeln(Result),
-        !.
-
 cli_api_init([]) :- cli_api_init([user]).
 cli_api_init([Format]) :-
         set_output_format(Format),
-        assertz(echo_output(X) :- writeln(X)),
         set_stream(user_input,tty(true)),
         set_stream(user_output,tty(true)),
         set_prolog_flag(toplevel_prompt,'CL ?-[~!]\n').
