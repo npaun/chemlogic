ba2da5c17df9b79e48f2f4f7b68fd378faf08cfd
Nicholas Paun <np@icebergsystems.ca>
2015-01-21 12:24:34 -0800

    We can now calculate molar mass.

---
+++ _expr_stoich/molar_mass.pl
@@ -0,0 +1,21 @@
+:- consult('../formula/compounder').
+
+add_sum(Element,Sub,MassIn,MassOut) :-
+	mass(Element,AtomicMass),
+	MassOut is MassIn + AtomicMass * Sub.
+
+	
+sum_part([],_,X,X).
+sum_part([[Sym,Sub]|PairS],GroupSub,Mass,MassF) :-
+	add_sum(Sym, Sub * GroupSub,Mass,Mass1),
+	sum_part(PairS,GroupSub,Mass1,MassF).
+
+sum_part(Sym,Sub,Mass,MassF) :-
+	add_sum(Sym, Sub, Mass, MassF).
+
+
+molar_mass([],X,X).
+
+molar_mass([[Sym,Sub]|PairS],Mass,MassF) :-
+	sum_part(Sym,Sub,Mass,Mass1),
+	molar_mass(PairS,Mass1,MassF).
