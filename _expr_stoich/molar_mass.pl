c5d05235cf4392ad4caf33cb8f4d7b9c8c09d05d
Nicholas Paun <np@icebergsystems.ca>
2015-01-22 14:53:09 -0800

    Added cuts to ensure that only the actual molar mass is produced as a solution.

--- _expr_stoich/molar_mass.pl
+++ _expr_stoich/molar_mass.pl
@@ -1,18 +1,20 @@
 :- consult('../formula/compounder').
 
 add_sum(Element,Sub,MassIn,MassOut) :-
-	mass(Element,AtomicMass),
+	mass(Element,AtomicMass),!,
 	MassOut is MassIn + AtomicMass * Sub.
 
 	
-sum_part([],_,X,X).
+sum_part([],_,X,X) :- !.
 sum_part([[Sym,Sub]|PairS],GroupSub,Mass,MassF) :-
 	add_sum(Sym, Sub * GroupSub,Mass,Mass1),
-	sum_part(PairS,GroupSub,Mass1,MassF).
+	sum_part(PairS,GroupSub,Mass1,MassF),
+	!.
 
 sum_part(Sym,Sub,Mass,MassF) :-
 	add_sum(Sym, Sub, Mass, MassF).
 
+molar_mass(Formula,MMass) :- molar_mass(Formula,0,MMass).
 
 molar_mass([],X,X).
 
