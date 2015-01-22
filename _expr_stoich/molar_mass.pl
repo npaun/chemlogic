5b9269451fb4eca8cfc982ee33bf38e3dcd46740
Nicholas Paun <np@icebergsystems.ca>
2015-01-22 14:53:59 -0800

    Added unit calculation support (mass, volume [gas only]).

--- _expr_stoich/molar_mass.pl
+++ _expr_stoich/molar_mass.pl
@@ -1,5 +1,34 @@
 :- consult('../formula/compounder').
 
+
+v_molar(22.414).
+
+%%% Mass units %%%%
+unit(Formula,Mass,g,Mol,mol) :-
+	molar_mass(Formula,MMass),
+	Mol /* mol */ is Mass /* g */ * 1 /* mol */ / MMass /* g */.
+
+unit(Formula,Mol,mol,Mass,g) :-
+	molar_mass(Formula,MMass),
+	Mass /* g */ is Mol /* mol */ * MMass /* g */ / 1 /* mol */.
+
+%%% Volume units %%%%
+/*** NOTE:
+Formula is assumed to represent a gas at STP (standard temperature and pressure)
+***/
+
+
+unit(_,Vol,'L',Mol,mol) :-
+	v_molar(Vm),
+	Vol /* L */ is Mol /* mol */ * Vm /* L */ / 1 /* mol */.
+
+unit(_,Mol,mol,Vol,'L') :-
+	v_molar(Vm),
+	Mol /* mol */ is Vol /* L */ * 1 /* mol */ / Vm /* L */.
+
+
+
+%%% Calculate the Molar Mass of a compound %%%
 add_sum(Element,Sub,MassIn,MassOut) :-
 	mass(Element,AtomicMass),!,
 	MassOut is MassIn + AtomicMass * Sub.
