024175033dee920a4e29dd2a1f32092463873d98
Nicholas Paun <np@icebergsystems.ca>
2015-03-07 12:43:58 -0800

    Modified sigfigs logic (still does not really work).

--- _expr_stoich/sigfigs.pl
+++ _expr_stoich/sigfigs.pl
@@ -1,14 +1,14 @@
 :- set_prolog_flag(double_quotes,chars).
 
-sigfigs(H,T) --> leading_zeros(_), whole(H), decimal_part(H,T).
+sigfigs(Sig,SigR) --> leading_zeros(_,_), whole(Sig,SigR0), decimal_part(Sig,SigR0,SigR).
 
-decimal_part([],T) -->  decimal_point, decimal_zeros(_), decimal(T).
-decimal_part(_,[H|T]) --> decimal_point, decimal_zeros(H), decimal(T).
-decimal_part(_,[]) --> "".
+decimal_part([],Sig,SigR) -->  decimal_point, decimal_zeros(_,_), decimal(Sig,SigR).
+decimal_part(_,Sig,SigR) --> decimal_point, decimal_zeros(Sig,SigR0), decimal(SigR0,SigR).
+decimal_part(_,Sig,Sig) --> "".
 
 
-leading_zeros(["0"|T]) --> "0", leading_zeros(T).
-leading_zeros([]) --> "".
+leading_zeros(Sig,SigR) --> zero_part(Sig,SigR0), leading_zeros(SigR0,SigR).
+leading_zeros(Sig,Sig) --> "".
 
 number("0") --> "0".
 number("1") --> "1".
@@ -21,11 +21,14 @@ number("7") --> "7".
 number("8") --> "8".
 number("9") --> "9".
 
-whole([H|T]) --> number(H), whole(T).
-whole([]) --> "".
+zero_part(["0",SigR],SigR) --> "0".
+num_part([Sig|SigR],SigR) --> number(Sig).
+
+whole([Sig|SigR],SigR) --> num_part(Sig,SigR0), whole(SigR0,SigR).
+whole(Sig,Sig) --> "".
 
 decimal_point --> ".".
 
-decimal_zeros(DZ) --> leading_zeros(DZ).
+decimal_zeros(DZ,DE) --> leading_zeros(DZ,DE).
 
-decimal(X) --> whole(X).
+decimal(X,Y) --> whole(X,Y).
