b51891bb110b5b147598c68f24b9c69b13a27fc1
Nicholas Paun <np@icebergsystems.ca>
2015-03-16 15:09:50 -0700

    Some correct rounding logic (in PHP).

---
+++ _expr_stoich/rounder.php
@@ -0,0 +1,43 @@
+<?php
+
+function magicround($num,$sf)
+	{
+		if ($num == 0)
+		  return(0);
+
+		$d = ceil(log10(abs($num)));
+		$power = $sf - $d;
+		$magnitude = pow(10,$power);
+		$shifted = round($num*$magnitude);
+		return($shifted/$magnitude);
+}
+
+function sf_print($num,$sf)
+ {
+   $rounded = magicround($num,$sf);
+   if (strpos($rounded,".") !== FALSE)
+    {
+      $current_sf = strlen($rounded) - 1;
+      $missing_sf = $sf - $current_sf;
+      if ($missing_sf > 0)
+      $ans = $rounded . str_repeat("0",$missing_sf);
+      else
+      $ans = $rounded;
+    }
+    else
+     {
+       $current_sf = strlen($rounded);
+       $missing_sf = $sf - $current_sf;
+       if ($missing_sf > 0)
+       $ans = $rounded . "." . str_repeat("0",$missing_sf);
+       else
+       $ans = $rounded;
+      }
+
+      return($ans);
+      }
+
+
+
+echo(sf_print($argv[1],$argv[2])."\n");
+
--- _expr_stoich/sigfigs.pl
+++ _expr_stoich/sigfigs.pl
@@ -1,35 +1,52 @@
 :- set_prolog_flag(double_quotes,chars).
-% This is a test.
-%
-sigfigs(Sig,SigR) --> leading_zeros(_,_), whole(Sig,SigR0), decimal_part(Sig,SigR0,SigR).
-
-decimal_part([],Sig,SigR) -->  decimal_point, decimal_zeros(_,_), decimal(Sig,SigR).
-decimal_part(_,Sig,SigR) --> decimal_point, decimal_zeros(Sig,SigR0), decimal(SigR0,SigR).
-decimal_part(_,Sig,Sig) --> "".
 
 
-leading_zeros(Sig,SigR) --> zero_part(Sig,SigR0), leading_zeros(SigR0,SigR).
-leading_zeros(Sig,Sig) --> "".
 
-number("0") --> "0".
-number("1") --> "1".
-number("2") --> "2".
-number("3") --> "3".
-number("4") --> "4".
-number("5") --> "5".
-number("6") --> "6".
-number("7") --> "7".
-number("8") --> "8".
-number("9") --> "9".
+sigfigs(Value,SF) :-
+	sig_number(SigDigits,[],Value,[]),
+	length(SigDigits,SF).
 
-zero_part(["0",SigR],SigR) --> "0".
-num_part([Sig|SigR],SigR) --> number(Sig).
 
-whole([Sig|SigR],SigR) --> num_part(Sig,SigR0), whole(SigR0,SigR).
-whole(Sig,Sig) --> "".
+sig_number(Digit,DigitR) --> leading_zeros(Zeros,ZerosR), 
+(
+	(new_digit(Digit,DigitR0) -> decimal_part(DigitR0,DigitR), !; decimal_part_nozero(Digit,DigitR), !);
+	{Digit = [0], DigitR = [], !}
+).
+
+
+decimal_part_nozero(Digit,DigitR) --> decimal_point, leading_zeros(Zeros,ZerosR), new_digit_tail(DigitCand,DigitCandR), 
+(
+	{DigitCand = []} -> {Digit = Zeros, ZerosR = [0|DigitR]}; {Digit = DigitCand, DigitR = DigitCandR}
+	).
+
+decimal_part(Digit,DigitR) --> decimal_point, leading_zeros(Digit,DigitR0), new_digit_tail(DigitR0,DigitR).
+decimal_part(Digit,Digit) --> decimal_point.
+decimal_part(Digit,Digit) --> "".
+
+
+
+leading_zeros([0|DigitR0],DigitR) --> "0", leading_zeros(DigitR0,DigitR).
+leading_zeros(Digit,Digit) --> "".
+
+
+
+new_digit([Digit|DigitR0],DigitR) --> digit(Digit),!, new_digit_tail(DigitR0,DigitR).
+new_digit_tail([Digit|DigitR0],DigitR) --> really_digit(Digit), new_digit_tail(DigitR0,DigitR), !.
+new_digit_tail(Digit,Digit) --> "",!.
+
+really_digit(Digit) --> digit(Digit).
+really_digit(Digit) --> zero(Digit).
+
+
+zero(0) --> "0".
+digit(1) --> "1".
+digit(2) --> "2".
+digit(3) --> "3".
+digit(4) --> "4".
+digit(5) --> "5".
+digit(6) --> "6".
+digit(7) --> "7".
+digit(8) --> "8".
+digit(9) --> "9".
 
 decimal_point --> ".".
-
-decimal_zeros(DZ,DE) --> leading_zeros(DZ,DE).
-
-decimal(X,Y) --> whole(X,Y).
