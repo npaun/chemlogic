3286f414822573bf173dbc9538ad1926ac1571f4
Nicholas Paun <np@icebergsystems.ca>
2015-03-23 15:17:14 -0700

    Now, we can correctly calculate the amount actually reacted/produced.

--- stoich/excess.pl
+++ stoich/excess.pl
@@ -1,45 +1,40 @@
-filter_query([],[],[],[]).
+filter_query([],[],[],[]) :- !.
 
 filter_query([Qty|QtyS],[Coeff|CoeffS],[Formula|FormulaS],[Struct|StructS]) :-
 	(
-		nonvar(Qty) ->
+		Qty = nil ->
 				(
-					Struct = [[Qty,Coeff,Formula]|StructS]
+
+					Struct = nil
 				);
 				(
-					Struct = StructS
+
+					Struct = [Qty,Coeff,Formula]
 				)
 	),
 	filter_query(QtyS,CoeffS,FormulaS,StructS).
 
 mol_ratio([],[],[]).
-mol_ratio([[QtyIn,CoeffIn,FormulaIn]|StructS],[MolRatio|MolRatioS],[[[Mol,mol],CoeffIn,FormulaIn]|NewStructS]) :-
-	calc_format(input,FormulaIn,QtyIn,[Mol,mol],_),
+mol_ratio([nil|StructS],[[nil,0]|MolRatioS],[nil|NewStructS]) :-
+	mol_ratio(StructS,MolRatioS,NewStructS).
+
+mol_ratio([[QtyIn,CoeffIn,FormulaIn]|StructS],[[MolRatio,SF]|MolRatioS],[[[Mol,mol],CoeffIn,FormulaIn]|NewStructS]) :-
+	calc_format(input,FormulaIn,QtyIn,[Mol,mol],SF),
 	MolRatio is Mol / CoeffIn,
 	mol_ratio(StructS,MolRatioS,NewStructS).
 
-limitant(StructS,NewStructS,LimitantStruct) :-
+limitant(StructS,NewStructS,LimitantStruct,LimitantSF) :-
 	mol_ratio(StructS,MolRatioS,NewStructS),
-	min_list(MolRatioS,MinMolRatio),
-	nth0(Index,MolRatioS,MinMolRatio),
+	min_member(MinMolRatio,MolRatioS),
+	MinMolRatio = [_,LimitantSF],
+	nth0(Index,MolRatioS,MinMolRatio), !,
 	nth0(Index,NewStructS,LimitantStruct).
 
-stoich_ui_excess(InGrammar,Equation,OutGrammar,Balanced,QtyS,ResultS) :-
-	balance_equation(InGrammar,Equation,OutGrammar,Balanced,CoeffS,FormulaS),
-	filter_query(QtyS,CoeffS,FormulaS,InStructS),
-	filter_query(ResultS,CoeffS,FormulaS,OutStructS),
-	limitant(InStructS,MolStructS,Limitant),
-	stoich_excess_calculate(MolStructS,Limitant,OutStructS).
-
-stoich_excess_calculate(_,[],[]).
-stoich_excess_calculate(_,Struct,[[QtyOut,CoeffOut,FormulaOut]|OutStructS]) :-
-	Struct = [[MolLim,mol],CoeffLim,_],
-	MolOut is MolLim * CoeffOut/CoeffLim,
-	calc_format(output,FormulaOut,[MolOut,mol],QtyOut,3),
-	stoich_excess_calculate(_,Struct,OutStructS).
-	
-
-
-%compute_excess([Total,Unit],[Actual,Unit],[Excess,Unit]) :-
+excess_calculate(_,_,[]) :- !.
+excess_calculate(Limitant,LimitantSF,[[QtyOut,CoeffOut,FormulaOut]|QueryS]) :-
+	Limitant = [[MolIn,mol],CoeffIn,_],
+	MolOut is MolIn * CoeffOut / CoeffIn,
+	calc_format(output,FormulaOut,[MolOut,mol],QtyOut,LimitantSF), !,
+	excess_calculate(Limitant,LimitantSF,QueryS).
 
 % vi: ft=prolog
