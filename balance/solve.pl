7920124e073474f2a80561aad76f9a66f5544b92
Nicholas Paun <np@icebergsystems.ca>
2015-03-26 15:46:49 -0700

    Improved the wording of some solver error messages.

--- balance/solve.pl
+++ balance/solve.pl
@@ -101,17 +101,17 @@ require_positive([Var|VarS]) :-
 guidance_general(positive,
 	'Your chemical equation has no solution with all coefficients positive.
 	 
-	 Your equation has been converted to the following system of linear equations: ').
+	 Your equation has been converted to the following system of linear equations, which cannot be solved: ').
 
 guidance_general(eval,
- 	'Your chemical equation is in the correct format, but is logically incorrect.
+ 	'Your chemical equation is in the correct format, but is logically invalid.
 	 Therefore, the equation cannot be balanced.
 
 	 e.g: CuCl2 + KBr --> CuBr + KCl
 	 This equation is in the correct format, but cannot be balanced becuase the copper ion cannot change its charge in this reaction.
 
 	 e.g: C4N2H --> CH + N
-	 This equation follows all of the rules, but is clearly nonsensical and it cannot be made to balance.
+	 This equation follows all of the rules, but is clearly nonsensical and cannot be made to balance.
 
 	 The following system of linear equations has no solution: ').
 
