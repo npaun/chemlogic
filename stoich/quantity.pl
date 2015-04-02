97b9077b3dcc54a262e3d024603a17b7a15d809b
Nicholas Paun <np@icebergsystems.ca>
2015-04-02 11:40:10 -0700

    Adjusted format for the quantity DCG to match the rest of Chemlogic.

--- stoich/quantity.pl
+++ stoich/quantity.pl
@@ -9,44 +9,44 @@
 :- use_module(sigfigs_number).
 :- set_prolog_flag(double_quotes,chars).
 
-quantity(qty([[Val,Unit]|Tail])) --> value(Val), " ", unit_sym(Unit), !, unit_tail(Unit,Tail), !.
+quantity([[Val,Unit]|Tail]) --> value(Val), " ", unit_sym(Unit), !, unit_tail(Unit,Tail), !.
 
-value(value(Val,SF)) --> 
+value([Val,SF]) --> 
 	({var(Val)} -> 
 		(
-			value_calc(value(Val,SF))
+			value_calc([Val,SF])
 		);
 		(
-			value_display(value(Val,SF))
+			value_display([Val,SF])
 		)
 	).
 
-value_calc(value(Val,SF)) --> number(SFDigits,[],Digits,[]), {length(SFDigits,SF), number_chars(Val,Digits)}.
-value_display(value(Val,_),H,T) :- format(chars(H,T),'~w',Val). % When performing output, it is much faster to just use SWI-Prolog's built-in conversion.
+value_calc([Val,SF]) --> number(SFDigits,[],Digits,[]), {length(SFDigits,SF), number_chars(Val,Digits)}.
+value_display([Val,_],H,T) :- format(chars(H,T),'~w',Val). % When performing output, it is much faster to just use SWI-Prolog's built-in conversion.
 
-query(query([Unit|Tail],Property)) --> unit_sym(Unit), !, property(Property), unit_tail(Unit,Tail), !.
+query([[Unit|Tail],Property]) --> unit_sym(Unit), !, property(Property), unit_tail(Unit,Tail), !.
 
-property(property(actual)) --> " reacted".
-property(property(actual)) --> " produced".
+property(actual) --> " reacted".
+property(actual) --> " produced".
 
-property(property(excess)) --> " excess".
+property(excess) --> " excess".
 
-property(property(actual)) --> "".
+property(actual) --> "".
 
-unit_sym(unit(g)) --> "g".
-unit_sym(unit('M')) --> "mol/L".
-unit_sym(unit(mol)) --> "mol".
-unit_sym(unit('L')) --> "L".
-unit_sym(unit('M')) --> "M".
+unit_sym(g) --> "g".
+unit_sym('M') --> "mol/L".
+unit_sym(mol) --> "mol".
+unit_sym('L') --> "L".
+unit_sym('M') --> "M".
 
-unit_tail(unit('L'),[[Val,unit('M')]]) --> " of ",  value(Val), " " ,unit_sym(unit('M')).
-unit_tail(unit('L'),[[Val,unit('M')]]) --> " (",  value(Val), " " ,unit_sym(unit('M')), ")".
-unit_tail(unit('M'),[[Val,unit('L')]]) --> " (",  value(Val), " " ,unit_sym(unit('L')), ")".
+unit_tail('L',[[Val,'M']]) --> " of ",  value(Val), " " ,unit_sym('M').
+unit_tail('L',[[Val,'M']]) --> " (",  value(Val), " " ,unit_sym('M'), ")".
+unit_tail('M',[[Val,'L']]) --> " (",  value(Val), " " ,unit_sym('L'), ")".
 
-unit_tail(unit('M'),[[Val,unit(mol)]]) --> " (",  value(Val), " " ,unit_sym(unit(mol)), ")".
-unit_tail(unit(mol),[[Val,unit('M')]]) --> " (",  value(Val), " " ,unit_sym(unit('M')), ")".
+unit_tail('M',[[Val,mol]]) --> " (",  value(Val), " " ,unit_sym(mol), ")".
+unit_tail(mol,[[Val,'M']]) --> " (",  value(Val), " " ,unit_sym('M'), ")".
 
-unit_tail(unit('L'),[[Val,unit(mol)]]) --> " (",  value(Val), " " ,unit_sym(unit(mol)), ")".
-unit_tail(unit(mol),[[Val,unit('L')]]) --> " (",  value(Val), " " ,unit_sym(unit('L')), ")".
+unit_tail('L',[[Val,mol]]) --> " (",  value(Val), " " ,unit_sym(mol), ")".
+unit_tail(mol,[[Val,'L']]) --> " (",  value(Val), " " ,unit_sym('L'), ")".
 
 unit_tail(_,[]) --> [].
