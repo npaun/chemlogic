43fa3dbf281c298a4bd65536bcb197a64dde83a8
Nicholas Paun <np@icebergsystems.ca>
2015-04-05 22:45:21 -0700

    Match neutralization, another double replacement ordering, synthesis, and decomposition reactions!!!

--- balance/reaction_types.pl
+++ balance/reaction_types.pl
@@ -1,13 +1,35 @@
-reaction_match(double_replacement,[
-					[
-						[[MSym1,_],[NMSym1,_]],
-						[[MSym2,_],[NMSym2,_]]
-					],
-					[
-						[[MSym1,_],[NMSym2,_]],
-						[[MSym2,_],[NMSym1,_]]
-					]
+
+reaction_match(neutralization,[
+					[[["H",_],[NMSym1,_]],[[MSym2,_],[[["O", 1], ["H", 1]], 1]]],
+					[[[MSym2,_],[NMSym1,_]],[["H", 2], ["O", 1]]]
 				]).
+
+reaction_match(neutralization,[
+					[[[MSym2,_],[[["O", 1], ["H", 1]], 1]],[["H",_],[NMSym1,_]]],
+					[[[MSym2,_],[NMSym1,_]],[["H", 2], ["O", 1]]]
+				]).
+
+reaction_match(double_replacement,[
+					[[[MSym1,_],[NMSym1,_]],[[MSym2,_],[NMSym2,_]]],
+					[[[MSym1,_],[NMSym2,_]],[[MSym2,_],[NMSym1,_]]]
+				]).
+
+reaction_match(double_replacement,[
+					[[[MSym1,_],[NMSym1,_]],[[MSym2,_],[NMSym2,_]]],
+					[[[MSym2,_],[NMSym1,_]],[[MSym1,_],[NMSym2,_]]]
+				]).
+
+reaction_match(synthesis,[
+				_,
+				[_]
+			]).
+
+reaction_match(decomposition,[
+					[_],
+					_
+			]).
+
+
 
 reaction_complete(double_replacement,_,ElementSideSet,_,[Product1,Product2],[ElementSideSet|_],[[Reactant1,Reactant2],[Product1,Product2]]) :-
 	ionic:rev_algo(Reactant1,[MSym1,MCharge1,NMSym1,NMCharge1]),
