85663bbfbcb94c263a4dc45f8cc4b6faef2b6e85
Nicholas Paun <np@icebergsystems.ca>
2015-04-08 21:13:26 -0700

    No reaction when the same anion or the same cation is present in both reactants.

--- balance/reaction_types.pl
+++ balance/reaction_types.pl
@@ -70,6 +70,8 @@ reaction_complete(neutralization,_,ElementSideSet,_,[Product1,Water],[ElementSid
 reaction_complete(double_replacement,_,ElementSideSet,_,[Product1,Product2],[ElementSideSet|_],[[Reactant1,Reactant2],[Product1,Product2]]) :-
 	ionic:rev_algo(Reactant1,[MSym1,MCharge1,NMSym1,NMCharge1]),
 	ionic:rev_algo(Reactant2,[MSym2,MCharge2,NMSym2,NMCharge2]),
+	MSym1 \= MSym2,
+	NMSym1 \= NMSym2,
 	ionic:fwd_algo(Product1,[MSym1,MCharge1,NMSym2,NMCharge2]),
 	ionic:fwd_algo(Product2,[MSym2,MCharge2,NMSym1,NMCharge1]).
 
