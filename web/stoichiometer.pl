c9f301e58e6e3571f4b5f9d233dc83eaf3a371d9
Nicholas Paun <np@icebergsystems.ca>
2015-04-10 22:58:30 -0700

    The Web Stoichiometer can actually perform queries in the background now!!!

--- web/stoichiometer.pl
+++ web/stoichiometer.pl
@@ -9,15 +9,16 @@
 
 
 
-stoichiometer_input(Request,Type,Input,OutputType) :-
+stoichiometer_input(Request,Type,Input,OutputType,QueryS) :-
 	http_parameters(Request,
 	[
 	type(Type, [ optional(true), oneof([symbolic,word]) ]),
 	stoichiometer_input(Input, [ optional(true) ]),
-	outputtype(OutputType, [ optional(true), oneof([symbolic,word]) ])
+	outputtype(OutputType, [ optional(true), oneof([symbolic,word]) ]),
+	queries_input(QueryS, [list(atom) ])
 	]).
 
-stoichiometer_html(Type,Input,OutputType,Solution) :-
+stoichiometer_html(Type,Input,OutputType,Solution,QueryS) :-
 	( var(Input) -> Input = []; true),
 	OptionS = [[symbolic,'Symbolic'],[word,'Word']],
 	chemweb_select_list(Type,OptionS,SelectList),
@@ -39,16 +40,26 @@ stoichiometer_nop(Solution) :-
 	Solution = '(1) Please select Symbolic or Word equation, depending on what you are entering, then enter it into the textbox. You can also select how the equation will be output. (2) After pressing ENTER, select the quantities you wish to determine from the dropdown menus.'.
 
 
-stoichiometer_process(Type,Input,OutputType,Solution) :-
-	atom_chars(Input,StringInput),
-	stoichiometer_do_process(Type,StringInput,OutputType,Solution).
+stoichiometer_queries_create([],[]).
+stoichiometer_queries_create([Unit|UnitS],[Query|QueryS]) :-
+	(Unit = nil ->
+		Query = nil;
+		Query = [[[[_, _], Unit]], actual]
+	),
+	stoichiometer_queries_create(UnitS,QueryS).
 
-stoichiometer_do_process(Type,StringInput,OutputType,Solution) :-
-	(balance_equation(Type,StringInput,OutputType,StringSolution,_,_,stoich,_), chemweb_to_html(StringSolution,Solution)) handle Solution.
+
+stoichiometer_process(Type,Input,OutputType,Solution,UnitS) :-
+	atom_chars(Input,StringInput),
+	stoichiometer_queries_create(UnitS,QueryS),
+	stoichiometer_do_process(Type,StringInput,OutputType,Solution,QueryS).
+
+stoichiometer_do_process(Type,StringInput,OutputType,Solution,QueryS) :-
+	(stoich_queries(Type,StringInput,OutputType,StringSolution,QueryS), chemweb_to_html(StringSolution,Solution)) handle Solution.
 
 
 stoichiometer_page(Request) :-
-	stoichiometer_input(Request,Type,Input,OutputType),
-	(nonvar(Input) -> stoichiometer_process(Type,Input,OutputType,Solution); stoichiometer_nop(Solution)),
+	stoichiometer_input(Request,Type,Input,OutputType,QueryS),
+	(nonvar(Input) -> stoichiometer_process(Type,Input,OutputType,Solution,QueryS); stoichiometer_nop(Solution)),
 
-	stoichiometer_html(Type,Input,OutputType,Solution).
+	stoichiometer_html(Type,Input,OutputType,Solution,QueryS).
