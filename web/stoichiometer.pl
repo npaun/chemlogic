cb77a30afd4134524497b64c960509b672d4ecb0
Nicholas Paun <np@icebergsystems.ca>
2015-04-11 00:01:53 -0700

    So close to done.

--- web/stoichiometer.pl
+++ web/stoichiometer.pl
@@ -24,7 +24,7 @@ stoichiometer_html(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS) :-
 	OptionS = [[symbolic,'Symbolic'],[word,'Word']],
 	chemweb_select_list(Type,OptionS,SelectList),
 	chemweb_select_list(OutputType,OptionS,OutputSelectList),
-
+	stoichiometer_query_list(UnitS,PropertyS,QuerySelect),
 	reply_html_page(chemlogic,title('Stoichiometer'),
 	[
 	h1(id(feature),'Stoichiometer'),
@@ -34,6 +34,7 @@ stoichiometer_html(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS) :-
 	select(name(outputtype),OutputSelectList)
 	]),
 	div(id(solution),Solution),
+	div(id(select),QuerySelect),
 	div(id(results),ResultS)
 	]
 	).
@@ -41,6 +42,13 @@ stoichiometer_html(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS) :-
 stoichiometer_nop(Solution) :-
 	Solution = '(1) Please select Symbolic or Word equation, depending on what you are entering, then enter it into the textbox. You can also select how the equation will be output. (2) After pressing ENTER, select the quantities you wish to determine from the dropdown menus.'.
 
+
+stoichiometer_query_list([],[],[]).
+stoichiometer_query_list([Unit|UnitS],[Property|PropertyS],[Select|SelectS]) :-
+		chemweb_select_list(Unit,[[nil,''],[g,g],['L','L (g)'],['L','L (aq)'],[mol,mol],['M','M']],UnitSelectList),
+		chemweb_select_list(Property,[[actual,''],[reacted,reacted],[produced,produced],[excess,excess]],PropertySelectList),
+		Select = span([select(name(units_input),UnitSelectList),select(name(properties_input),PropertySelectList)]),
+		stoichiometer_query_list(UnitS,PropertyS,SelectS).
 
 stoichiometer_queries_create([],[],[]).
 stoichiometer_queries_create([Unit|UnitS],[Property|PropertyS],[Query|QueryS]) :-
@@ -73,9 +81,23 @@ stoichiometer_process(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS) :-
 stoichiometer_do_process(Type,StringInput,OutputType,Solution,QueryS) :-
 	(stoich_queries(Type,StringInput,OutputType,StringSolution,QueryS), chemweb_to_html(StringSolution,Solution)) handle Solution.
 
+stoichiometer_nop_select(Type,Input,OutputType,Solution,QtyS) :-
+	atom_chars(Input,StringInput),
+	stoichiometer_do_nop_select(Type,StringInput,OutputType,Solution,QtyS).
+
+stoichiometer_do_nop_select(Type,StringInput,OutputType,Solution,QtyS) :-
+		(balance_equation(Type,StringInput,OutputType,StringSolution,_,_,stoich,QtyS), chemweb_to_html(StringSolution,Solution)) handle Solution.
+	
+
 
 stoichiometer_page(Request) :-
 	stoichiometer_input(Request,Type,Input,OutputType,UnitS,PropertyS),
-	(nonvar(Input) -> stoichiometer_process(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS); stoichiometer_nop(Solution)),
+	(\+ UnitS = [] -> 
+		(debug(chemlogic,'~w~n','Yo mama!'), stoichiometer_process(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS)); 
+		(nonvar(Input) ->
+			stoichiometer_nop_select(Type,Input,OutputType,Solution,UnitS);
+			stoichiometer_nop(Solution)
+		)
+	),
 
 	stoichiometer_html(Type,Input,OutputType,Solution,UnitS,PropertyS,ResultS).
